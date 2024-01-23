c82cea13c5e95d897a4aa67cc3f13bc7ff7ac7c1
Anthony Ding <dinganthony@gmail.com>
2024-01-22 21:20:16 -0800

    Remove blog blurb on main page

--- type.js
+++ type.js
@@ -1,9 +1,8 @@
 new TypeIt("#typeit", {
     speed: 10,
     cursor: false,
-    strings: ["Hi! I'm Anthony Ding, a software engineer at Amazon, based in Seattle, WA, USA. I work on enhancing Alexa's contextual understanding so that Amazon customers have a natural and conversational shopping experience.<br/>",
+    strings: ["Hi! I'm Anthony Ding, a software engineer at Amazon, based in Cupertino, CA, USA. I work on enhancing Alexa's contextual understanding so that Amazon customers have a natural and conversational shopping experience.<br/>",
               "Previously, I did my undergrad at UC Berkeley, majoring in Electrical Engineering and Computer Science. At Berkeley, I fell in love with teaching through the <a href='https://csmentors.berkeley.edu/'>Computer Science Mentors</a> project.<br/>",
-              "I'm in the process of creating a soccer data analysis blog, but it isn't quite ready yet!<br/>",
               "Let's get in touch:"
     ],
     afterComplete: async () => {
