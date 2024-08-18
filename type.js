13a60992ffc1cbce7bd859ae120187e7848892d8
Anthony Ding <dinganthony@gmail.com>
2024-08-17 18:33:03 -0700

    Update bio

--- type.js
+++ type.js
@@ -1,8 +1,8 @@
 new TypeIt("#typeit", {
     speed: 10,
     cursor: false,
-    strings: ["Hi! I'm Anthony Ding, a software engineer at Amazon, based in Cupertino, CA, USA. I work on enhancing Alexa's contextual understanding so that Amazon customers have a natural and conversational shopping experience.<br/>",
-              "Previously, I did my undergrad at UC Berkeley, majoring in Electrical Engineering and Computer Science. At Berkeley, I fell in love with teaching through the <a href='https://csmentors.berkeley.edu/'>Computer Science Mentors</a> project.<br/>",
+    strings: ["Hi! I'm Anthony Ding, and I'm a Masters student studying Computer Science at Columbia University.<br/>",
+              "Previously, I worked as a software engineer at Amazon Alexa and did my undergrad at UC Berkeley.",
               "Let's get in touch:"
     ],
     afterComplete: async () => {
