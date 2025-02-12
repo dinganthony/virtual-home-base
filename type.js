ed912d473bbee50b7369701001b923114be21108
Anthony Ding <dinganthony@gmail.com>
2025-02-12 07:00:53 -0800

    Update type.js

--- type.js
+++ type.js
@@ -1,7 +1,7 @@
 new TypeIt("#typeit", {
     speed: 10,
     cursor: false,
-    strings: ["Hi! I'm Anthony Ding, and I'm a Masters student at Columbia University, studying Computer Science with a focus on machine learning.<br/>",
+    strings: ["Hi! I'm Anthony Ding, and I'm a software engineer at Integuru, a YC-backed startup building integrations with AI.<br/>",
               "Previously, I worked as a software engineer at Amazon Alexa and did my undergrad at UC Berkeley, majoring in Electrical Engineering and Computer Science.<br/>",
               "Let's get in touch:"
     ],
